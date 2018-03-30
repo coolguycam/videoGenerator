0be81671d09cb41c778254250dce210008788f89
Cameron Dimitroff <hawtrods@gmail.com>
2018-03-29 21:50:19 -0700

    autoplay

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,52 +1,61 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    // var arr = ['<iframe width="560" height="315" src="https://www.youtube.com/embed/AM33T7q4XbI" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
-    // ""];
-var videoSources = ["https://www.youtube.com/embed/fGqdIPer-ms", "https://www.youtube.com/embed/fGqdIPer-ms", "https://www.youtube.com/embed/6YzGOq42zLk",
-    "https://www.youtube.com/embed/EEMcd48UwMI", "https://www.youtube.com/embed/vHb0ysdvs3Q", "https://www.youtube.com/embed/2FdP0eS47ts", 
-    "https://www.youtube.com/embed/aFUzvbkEvRk", "https://www.youtube.com/embed/rkB5bM_54sc", "https://www.youtube.com/embed/Nx94936Hl5Q", 
-    "https://www.youtube.com/embed/KyZDvZmvhik", "https://www.youtube.com/embed/_1gShXIO6pk", "https://www.youtube.com/embed/2CG5XMuz_nY?start=45",
-    "https://www.youtube.com/embed/IJWlBfo5Oj0", "https://www.youtube.com/embed/qKax7euEM5Q", "https://www.youtube.com/embed/8IV___Motnc",
-    "https://www.youtube.com/embed/A8tIuYwFcS0", "https://www.youtube.com/embed/482lqzBFS38",
-    "https://www.youtube.com/embed/gtCcOGlGVT8", "https://www.youtube.com/embed/eEfy3KkEkTE",
-    "https://www.youtube.com/embed/0Vkqx4nRLdc", "https://www.youtube.com/embed/KoQy_p3vNb8",
-    "https://www.youtube.com/embed/Ev0jl6RS5Eo", "https://www.youtube.com/embed/7SsAYhC9UuE"]
-    // var current = 0;
+    // Empty array for pushing new videos:
+
+    var newVidz = [];
+
+    // Sources for the iframes:
+
+    var videoSources = ["https://www.youtube.com/embed/6YzGOq42zLk?autoplay=1", 
+                        "https://www.youtube.com/embed/EEMcd48UwMI?autoplay=1", 
+                        "https://www.youtube.com/embed/vHb0ysdvs3Q?autoplay=1", 
+                        "https://www.youtube.com/embed/2FdP0eS47ts?autoplay=1", 
+                        "https://www.youtube.com/embed/aFUzvbkEvRk?autoplay=1", 
+                        "https://www.youtube.com/embed/rkB5bM_54sc?autoplay=1", 
+                        "https://www.youtube.com/embed/Nx94936Hl5Q?autoplay=1", 
+                        "https://www.youtube.com/embed/KyZDvZmvhik?autoplay=1", 
+                        "https://www.youtube.com/embed/_1gShXIO6pk?autoplay=1",                         
+                        "https://www.youtube.com/embed/IJWlBfo5Oj0?autoplay=1", 
+                        "https://www.youtube.com/embed/qKax7euEM5Q?autoplay=1", 
+                        "https://www.youtube.com/embed/8IV___Motnc?autoplay=1", 
+                        "https://www.youtube.com/embed/A8tIuYwFcS0?autoplay=1", 
+                        "https://www.youtube.com/embed/482lqzBFS38?autoplay=1", 
+                        "https://www.youtube.com/embed/AqZceAQSJvc?autoplay=1", 
+                        "https://www.youtube.com/embed/gtCcOGlGVT8?autoplay=1", 
+                        "https://www.youtube.com/embed/eEfy3KkEkTE?autoplay=1", 
+                        "https://www.youtube.com/embed/0Vkqx4nRLdc?autoplay=1", 
+                        "https://www.youtube.com/embed/KoQy_p3vNb8?autoplay=1", 
+                        "https://www.youtube.com/embed/Ev0jl6RS5Eo?autoplay=1", 
+                        "https://www.youtube.com/embed/7SsAYhC9UuE?autoplay=1",
+                        "https://www.youtube.com/embed/2CG5XMuz_nY?start=45",
+                        "https://www.youtube.com/embed/5NlHYdxi85w?start=164"]
+
     console.log(videoSources);
-    
-    $("button").on("click", function() {
-         var video = videoSources [Math.floor(Math.random() * videoSources.length)];
-         console.log(video);
-         $("button").html("Keep Clicking");
-         $("#lead").attr("src",video);
+
+
+    // var current = 0;
+
+   
+    // Button randomly chooses new video and push to html:
+
+    $("button").on("click", function () {
+        var video = videoSources[Math.floor(Math.random() * videoSources.length)];
+        console.log(video);
+        $("button").html("Keep Clicking");
+        $("#lead").attr("src", video);
     });
 
-    // function myNewSrc() {
-    //     var myVideo = document.getElementsByTagName('video')[0];
-    //     .src = videoSources[currentIndex];
-    //     myVideo.load();
-    // }
+    // Function used to add autoplay capabilities to videos and push to sources:
 
-    // myNewSrc(0);
+     function newVideo(x) {
+         for (i = 0; i < videoSources.length; i++) {
+             var newSources = x[i] + "?autoplay=1";
+             newVidz.push(newSources);
+             console.log(newVidz);
+         }
+     }
+     // newVideo(videoSources);
 
-    
 
-    //       var currentIndex = 0;
-    // // listener function changes src
-    //       function myNewSrc() {
-    //           var myVideo = document.getElementsByTagName('video')[0];
-    //           myVideo.src = videoSources[currentIndex];
-    //           myVideo.load();
-    //       }
 
-         
-    // // add a listener function to the ended event
-    //       function myAddListener(){
-    //           var myVideo = document.getElementsByTagName('video')[0];
-    //           currentIndex = (currentIndex+1) % videoSources.length;
-    //           myVideo.src = videoSources[currentIndex];
-    //           myVideo.addEventListener('ended', myNewSrc, false);
-              
-    //       }
-  
 });
