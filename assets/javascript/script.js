5ff1118c23cd9f9dcb9f90b69140e096d8c4a739
Cameron Dimitroff <hawtrods@gmail.com>
2018-03-30 22:08:12 -0700

    into to new script incorporating AJAX

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,5 @@
+console.log("hey");
+
 $(document).ready(function () {
 
     // Empty array for pushing new videos:
@@ -27,34 +29,58 @@ $(document).ready(function () {
                         "https://www.youtube.com/embed/KoQy_p3vNb8?autoplay=1", 
                         "https://www.youtube.com/embed/Ev0jl6RS5Eo?autoplay=1", 
                         "https://www.youtube.com/embed/7SsAYhC9UuE?autoplay=1",
+                        "https://www.youtube.com/embed/rVeMiVU77wo?autoplay=1",
                         "https://www.youtube.com/embed/2CG5XMuz_nY?start=45",
-                        "https://www.youtube.com/embed/5NlHYdxi85w?start=164"]
+                        "https://www.youtube.com/embed/5NlHYdxi85w?start=164",
+                        "https://www.youtube.com/embed/NhheiPTdZCw?autoplay=1"]
+
+    // Different phrases for when you click the button
+
+    var clickOptions = ["Keep Going", "Keep Clicking","Keep It Real", "Move Foward", "Next Vid", "What's Next", "Click Here", "Press Button", "Enjoy Vid",
+                        "Raise Roof", "Rip It Hard", "Shorty McDangles", "WagWan", "Stay Chill", "Keep It Cool", "Remain Frosty", "Move It Along", "next",
+                        ];
+                       
+
 
     console.log(videoSources);
 
+    // Function used to add autoplay capabilities to videos and push to sources:
 
-    // var current = 0;
+    // function newVideo(x) {
+    //     for (i = 0; i < videoSources.length; i++) {
+    //         var newSources = x[i] + "?autoplay=1";
+    //         newVidz.push(newSources);
+    //         console.log(newVidz);
+    //     }
+    // }
 
+    // newVideo(videoSources);
+
+    // API Key ------ AIzaSyAuXPcEOJndYVhYw_uAzBkCrEFGuFpeOZ4
+
+    //??????????????
+
+//     function playVideo(){
+//         var video = $('#lead');
+//         video.play();
+//         video.addEventListener('ended',function(){
+//         window.location = 'http://www.google.com';
+//     });
+// }
+    // ?????????????????
    
+
+
     // Button randomly chooses new video and push to html:
 
     $("button").on("click", function () {
         var video = videoSources[Math.floor(Math.random() * videoSources.length)];
         console.log(video);
-        $("button").html("Keep Clicking");
+        $("button").html(clickOptions[Math.floor(Math.random() * clickOptions.length)]);
         $("#lead").attr("src", video);
     });
 
-    // Function used to add autoplay capabilities to videos and push to sources:
-
-     function newVideo(x) {
-         for (i = 0; i < videoSources.length; i++) {
-             var newSources = x[i] + "?autoplay=1";
-             newVidz.push(newSources);
-             console.log(newVidz);
-         }
-     }
-     // newVideo(videoSources);
+   
 
 
 
---
+++ assets/javascript/scriptOne.js
@@ -0,0 +1,47 @@
+var gap = 
+
+function addResult(title, video, artist) {
+    $("#lead").append($(
+
+    `
+        <div>
+            <h2>${title}</h2>
+            <iframe width="560" height="315" src="${video}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            <h3>${artist}</h3>
+        </div>
+    `
+))};
+
+function onClientLoad() {
+    gapi.client.load("youtube", "v3", onYouTubeApiLoad);
+}
+
+function onYouTubeApiLoad() {
+    gapi.client.setApiKey('AIzaSyAuXPcEOJndYVhYw_uAzBkCrEFGuFpeOZ4');
+    search();
+}
+
+function search() {
+    var request = gapi.client.youtube.search.list({
+        part: 'url'
+    });
+
+    request.execute(onSearchResponse);
+}
+
+function onSearchResponse(response) {
+    showResponse(response);
+}
+
+// $(document).on("click", "#random-btn" function (x) {
+//     x.preventDefault();
+    
+//     let queryURL ="https://www.gooleapis.com/youtube/v3/pl"
+
+    
+
+
+
+
+
+// });
